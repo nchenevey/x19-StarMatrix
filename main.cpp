06300b240a1b13826fd5f703e6807e0573b7f92c
nchenevey <nchenevey1@mail.csuchico.edu>
2017-10-18 17:37:28 -0700

    Asks for rows then columns, if both are greater than 0, outputs stars with desired rows and columns

--- main.cpp
+++ main.cpp
@@ -10,39 +10,22 @@ vector <vector <char> > starMatrix;
 int rowsInput;
 int colsInput;
 
-
-cout<<"Please enter the desired number of rows: ";
+cout<<"Enter rows and columns of stars: "<<endl;
 cin>>rowsInput;
-
-while(rowsInput <= 0)
-{
-    cout<<"Please enter a row size greater than 0: ";
-    cin>>rowsInput;
-}
-
-
-
-cout<<"Please enter the desired number of columns: ";
 cin>>colsInput;
-
-while(colsInput <= 0)
+if(rowsInput > 0 && colsInput > 0)
 {
-    cout<<"Please enter a column size greater than 0: ";
-    cin>>colsInput;
-}
-
-
-for(int rows = 0; rows < rowsInput; rows++)
-{
-    starMatrix.resize(rowsInput);
-    for(int cols = 0; cols < colsInput; cols++)
+    for(int rows = 0; rows < rowsInput; rows++)
     {
-        starMatrix[rows].resize(colsInput);
-        starMatrix[rows][cols] = '*';
-        cout<<starMatrix[rows][cols];
+        starMatrix.resize(rowsInput);
+        for(int cols = 0; cols < colsInput; cols++)
+        {
+            starMatrix[rows].resize(colsInput);
+            starMatrix[rows][cols] = '*';
+            cout<<starMatrix[rows][cols];
+        }
+        cout<<endl;
     }
-    cout<<endl;
 }
-
 return 0;
 }
